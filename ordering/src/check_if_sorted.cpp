094442c3255bb93e60eff50d921344028fbbe70a
Placido Fernandez <pmail314@gmail.com>
2018-04-26 19:43:16 +0200

    added sse2 test

--- ordering/src/check_if_sorted.cpp
+++ ordering/src/check_if_sorted.cpp
@@ -4,6 +4,8 @@
 
 #include <iostream>
 
+#include <immintrin.h>
+
 bool is_sorted(const int32_t* input, size_t n) {
   if (n < 2) {
     return true;
@@ -11,6 +13,57 @@ bool is_sorted(const int32_t* input, size_t n) {
 
   for (size_t i=0; i < n - 1; i++) {
     if (input[i] > input[i + 1])
+      return false;
+  }
+
+  return true;
+}
+
+bool is_sorted_sse(const int32_t* input, size_t n) {
+  size_t i = 0;
+
+  if (n > 4) {
+    for (/**/; i < n - 4; i +=4) {
+      const __m128i curr = _mm_loadu_si128(reinterpret_cast<const __m128i*>(input + i));
+      const __m128i next = _mm_loadu_si128(reinterpret_cast<const __m128i*>(input + i + 1));
+
+      const __m128i mask = _mm_cmpgt_epi32(curr, next);
+      if (!_mm_test_all_zeros(mask, mask)) {
+        return false;
+      }
+
+    }
+  }
+
+  // normal checking
+  for (/**/; i + 1 < n; i++) {
+    if (input[i] > input[i + 1])
+      return false;
+  }
+}
+
+bool is_sorted_sse_2(int32_t* a, size_t n) {
+
+  size_t i = 0;
+  if (n >= 8) {
+    __m128i chunk0 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(a));
+    do {
+      const __m128i chunk1 = _mm_loadu_si128(reinterpret_cast<const __m128i*>(a + i + 4));
+      const __m128i curr = chunk0;
+      const __m128i next = _mm_alignr_epi8(chunk1, chunk0, 4);
+
+      const __m128i mask = _mm_cmpgt_epi32(curr, next);
+      if (!_mm_test_all_zeros(mask, mask)) {
+        return false;
+      }
+
+      chunk0 = chunk1;
+      i += 4;
+    } while (i < n - 4);
+  }
+
+  for (/**/; i + 1 < n; i++) {
+    if (a[i] > a[i + 1])
       return false;
   }
 
@@ -30,8 +83,27 @@ int main() {
     input[i] = i;
   }
 
-  ok = is_sorted(&input[0], max_size);
 
-  std::cout << std::boolalpha << ok << std::endl;
+  ok = is_sorted(&input[0], max_size);
+  std::cout << "no vec: " << std::boolalpha << ok << std::endl;
+  ok = is_sorted_sse(&input[0], max_size);
+  std::cout << "sse vec: " << std::boolalpha << ok << std::endl;
+  ok = is_sorted_sse_2(&input[0], max_size);
+  std::cout << "sse2 vec: " << std::boolalpha << ok << std::endl;
+  
+
+
+  std::cout << "\nNot ordered\n" << std::endl;
+
+  // not ordered
+  input[max_size/2] = 0;
+  ok = is_sorted(&input[0], max_size);
+  std::cout << "no vec: " << std::boolalpha << ok << std::endl;
+  ok = is_sorted_sse(&input[0], max_size);
+  std::cout << "sse vec: " << std::boolalpha << ok << std::endl;
+  ok = is_sorted_sse_2(&input[0], max_size);
+  std::cout << "sse2 vec: " << std::boolalpha << ok << std::endl;
+
+
 
 }
